657a3d5ba719fb3ed66af9fb08b7b7bbc86e6de5
dpisani <dean.a.pisani@gmail.com>
2020-12-12 09:43:46 +1100

    Fill gaps in missing JSDoc for the Node component

--- src/components/node/index.js
+++ src/components/node/index.js
@@ -11,22 +11,60 @@ export default class Node extends NamedComponent {
     super({ indexName: 'nodes', defaultProperties: { children: [] } });
   }
 
+  /**
+   * Adds a child node under this one
+   *
+   * @param {Node} child
+   * @returns {Node} this
+   * @memberof Node
+   */
   addChild(child) {
     this.properties.children.push(child);
 
     return this;
   }
 
+  /**
+   * Sets the translation of this node along the x, y, and z axes.
+   * Should not be used in conjunction with the matrix property.
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @returns {Node} this
+   * @memberof Node
+   */
   translation(x, y, z) {
     this.properties.translation = [x, y, z];
     return this;
   }
 
+  /**
+   * Sets the node's unit quaternion rotation in the order (x, y, z, w), where w is the scalar.
+   * Should not be used in conjunction with the matrix property.
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @param {number} w
+   * @returns {Node} this
+   * @memberof Node
+   */
   rotation(x, y, z, w) {
     this.properties.rotation = [x, y, z, w];
     return this;
   }
 
+  /**
+   * Sets the node's non-uniform scale, given as the scaling factors along the x, y, and z axes.
+   * Should not be used in conjunction with the matrix property.
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @returns {Node} this
+   * @memberof Node
+   */
   scale(x, y, z) {
     this.properties.scale = [x, y, z];
     return this;
